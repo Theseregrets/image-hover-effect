c4c727987b0e9d91192780c4c743b864d11f54e9
Theseregrets <rishitashaw49@gmail.com>
2021-05-21 07:38:10 +0530

    custom cursor added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Gallery from "../components/gallery";
+import CustomCursor from "../custom-cursor";
 import "../styles/home.scss";
 
 const Index = () => {
   return (
     <div className="main-container">
+      <CustomCursor />
       <Gallery />
     </div>
   );
