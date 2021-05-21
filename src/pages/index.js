a895c5a71fbc55f155aebb0b0a5928d12ba82de2
Theseregrets <rishitashaw49@gmail.com>
2021-05-21 08:19:02 +0530

    cursor changed a lil bit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Gallery from "../components/gallery";
 import CustomCursor from "../custom-cursor";
+import CursorManager from "../custom-cursor/CursorManager";
 import "../styles/home.scss";
 
 const Index = () => {
   return (
-    <div className="main-container">
-      <CustomCursor />
-      <Gallery />
-    </div>
+    <CursorManager>
+      <div className="main-container">
+        <CustomCursor />
+        <Gallery />
+      </div>
+    </CursorManager>
   );
 };
 export default Index;
